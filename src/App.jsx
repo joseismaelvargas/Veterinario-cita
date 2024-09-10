8079bd12e79f0197c28aaf6b91b841312e29685a
ismael <joseismaelvargas@users.noreply.github.com>
2024-09-09 22:35:34 -0300

    Se hiso el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
 
   return (
     <>
+    <h1 className="text-center my-3"> Administrar Pacientes de Veterinaria </h1>
         <Formulariocita></Formulariocita>
     </>
   )
--- src/components/Formulariocita.jsx
+++ src/components/Formulariocita.jsx
@@ -1,19 +1,83 @@
+import { Button } from 'bootstrap';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import{ v4 as uuidv4}from'uuid'
 import Form from 'react-bootstrap/Form';
+import { useForm } from "react-hook-form"
+import { useState } from 'react';
 
 function Formulariocita() {
+    
+    const { register, handleSubmit ,formState:{errors}} = useForm();
+    const [array,setarray]=useState([])
+    // const[local,setlocal]=useState
+    localStorage.setItem("veterinaria",JSON.stringify(array))
+    const agregar=(data,e)=>{
+          e.preventDefault()
+            e.target.reset()
+
+        let citas={
+            nombre:data.nombre,
+            dueño:data.dueño,
+            sintoma:data.sintoma,
+            fecha:data.fecha,
+            hora:data.hora,
+            id:uuidv4()
+        }
+        setarray([citas,...array])
+        
+        
+    }
+    console.log(array)
   return (
     <>
+    <Form onSubmit={handleSubmit(agregar)}>
       <FloatingLabel
         controlId="floatingInput"
-        label="Email address"
-        className="container mb-3 my-3"
+        label="Nombre de mascota"
+        className="container mb-3 my-3 "
       >
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control type="text" placeholder="Nombre de mascota" name='nombre' {...register("nombre",{
+            required:{value:true,message:"Este campo debe estar lleno"},
+            minLength:{value:3,message:"Nose puede Especificar Nombre Mascota"},
+           
+        })} />
+         <span className='text-danger' >{errors.nombre&&errors.nombre.message}</span> 
       </FloatingLabel>
-      <FloatingLabel controlId="floatingPassword" label="Password" className='container'>
-        <Form.Control type="password" placeholder="Password" />
+      
+      <FloatingLabel controlId="floatingPassword" label="Nombre de dueño" className='container'>
+        <Form.Control type="text" placeholder="Nombre de dueño"  name='dueño' {...register("dueño",{
+            required:{value:true,message:"Este campo debe estar lleno"},
+            minLength:{value:3,message:"No se puede espesificar Nombre Dueño"}
+        })}/>
+        <span className='text-danger' >{errors.dueño&&errors.dueño.message}</span> 
       </FloatingLabel>
+
+      <FloatingLabel label="Fecha" className='container my-3' >
+        <Form.Control type='date' placeholder='Fecha' name='fecha' {...register("fecha",{
+            required:{value:true,message:"Ponga Fecha por favor"}
+        })}/>
+        <span className='text-danger' >{errors.fecha&&errors.fecha.message}</span> 
+      </FloatingLabel>
+
+      <FloatingLabel label="Hora de cita" className='container my-3'>
+        <Form.Control type='time' placeholder='Hora de cita' name='hora' {...register("hora",{
+            required:{value:true,message:"Ponga Hora por favor"}
+        })}></Form.Control>
+        <span className='text-danger' >{errors.hora&&errors.hora.message}</span> 
+      </FloatingLabel>
+      
+      <FloatingLabel label="Sintoma" className='container my-3'>
+        <Form.Control type='text' placeholder='Sintoma' name='sintoma'{...register("sintoma",{
+            required:{value:true,message:"Este campo debe estar lleno"},
+            minLength:{value:5,message:"Sea mas especifico"}
+        })}></Form.Control>
+        <span className='text-danger' >{errors.sintoma&&errors.sintoma.message}</span> 
+      </FloatingLabel>
+      <div className='d-flex justify-content-center '>
+        <button className='buttonenviar btn btn-primary my-3' type='Submit'>Agregar Cita</button>
+      </div>
+     
+      </Form>
     </>
   );
 }
